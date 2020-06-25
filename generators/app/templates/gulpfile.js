a3ae580c7d25067f2576c67c9608be712c3d2555
lijie <14734576@qq.com>
2020-06-25 11:11:18 +0800

    up

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -1,15 +1,27 @@
-const { src, dest, series, watch } = require('gulp')
+const { src, dest, lastRun, series, watch } = require('gulp')
 const plugins = require('gulp-load-plugins')()
 const del = require('del')
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload
+const imagemin = require('gulp-imagemin');
+const svgo = require('gulp-svgo');
+const babel = require('gulp-babel');
+const browserify = require('gulp-browserify');
+const rename = require("gulp-rename");
 
 function js(cb) {
     src('src/js/*.js')
+        .pipe(babel())
+        // .pipe(browserify({
+        //     insertGlobals: true
+        // }))
+        .pipe(dest('./dist/js'))
         .pipe(plugins.uglify())
+        .pipe(rename({ extname: '.min.js' }))
         .pipe(dest('./dist/js'))
         .pipe(reload({ stream: true }))
     cb()
+    console.log(process.env.NODE_ENV);
 }
 
 function css(cb) {
@@ -24,9 +36,22 @@ function css(cb) {
     cb()
 }
 
+function images(cb) {
+    src('./src/images/*', { since: lastRun(images) })
+        .pipe(imagemin())
+        .pipe(dest('./dist/images'))
+    cb()
+}
+
+function svgs(cb) {
+    src('./src/svg/**')
+        .pipe(svgo())
+        .pipe(dest('./dist/svg'))
+    cb()
+}
+
 function html(cb) {
-    src('./index.html')
-        // .pipe(validator())
+    src('index.html')
         .pipe(dest('./dist'))
         .pipe(reload({ stream: true }))
     cb()
@@ -35,20 +60,22 @@ function html(cb) {
 function watcher(cb) {
     watch('src/js/*.js', js)
     watch('src/css/*.sass', css)
+    watch('src/images/**', images)
+    watch('src/svg/**', svgs)
     watch('*.html', html)
     cb()
 
 }
 
-function clean(cb) {
-    del('./dist')
+async function clean(cb) {
+    await del('./dist')
     cb()
 }
 
 function serve(cb) {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./dist"
         }
     });
     cb()
@@ -59,11 +86,15 @@ exports.styles = css
 exports.watcher = watcher
 exports.html = html
 exports.serve = serve
+exports.images = images
+exports.svgs = svgs
 exports.default = series([
     clean,
+    images,
+    svgs,
     js,
     html,
     css,
+    watcher,
     serve,
-    watcher
 ])
--- generators/app/templates/src/js/index.js
+++ generators/app/templates/src/js/index.js
@@ -1,2 +1,14 @@
 console.log('gulp log');
 
+var a =()=>{
+    console.log('a');
+}
+a()
+var promise = ()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve('success')
+        }, 1000);
+    })
+}
+promise()
